cb54930058ed2ca3e4e39198c91db7754417d7d1
Lucas Rosas <lucasroscun@gmail.com>
2026-04-20 22:19:18 -0300

    bugfix: Added Step 3 && 7 to Extract sequence

--- python/projects/run_apollo_sequence_contacts.py
+++ python/projects/run_apollo_sequence_contacts.py
@@ -36,9 +36,11 @@ NOMES_SEQUENCES = [
 STEPS_LABELS = [
     "Step: 1",
     "Step: 2",
+    "Step: 3",
     "Step: 4",
     "Step: 5",
-    "Step: 6"
+    "Step: 6",
+    "Step: 7",
 ]
 
 # 3. Configurações de Ambiente
